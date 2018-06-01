9c56ab7b5ebd6c47e0cc951e5276421d24ba8fcc
vode <vode@vodedeMacBook-Pro.local>
2018-06-01 15:11:20 +0800

    init commit

--- server.py
+++ server.py
@@ -26,6 +26,25 @@ def index():
     }]
   )
 
+@app.route('/test', methods=['POST'])
+def index():
+  print(port)
+  data = json.loads(request.get_data())
+
+  # FETCH THE CRYPTO NAME
+  crypto_name = "BTC"
+
+  # FETCH BTC/USD/EUR PRICES
+  r = requests.get("https://min-api.cryptocompare.com/data/price?fsym="+crypto_name+"&tsyms=BTC,USD,EUR")
+
+  return jsonify(
+    status=200,
+    replies=[{
+      'type': 'text',
+      'content': 'The price of %s is :\n%f BTC, \n%f USD, and \n%f EUR.' % (crypto_name, r.json()['BTC'], r.json()['USD'], r.json()['EUR'])
+    }]
+  )
+
 @app.route('/errors', methods=['POST'])
 def errors():
   print(json.loads(request.get_data()))
