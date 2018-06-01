7a3d375b607102fa4444e5f5633ea2ab7f09d452
vode <vode@vodedeMacBook-Pro.local>
2018-06-01 15:58:42 +0800

    fix

--- server.py
+++ server.py
@@ -45,6 +45,23 @@ def test():
     }]
   )
 
+@app.route('/test_view', methods=['GET'])
+def test_view():
+  
+  # FETCH THE CRYPTO NAME
+  crypto_name = "BTC"
+
+  # FETCH BTC/USD/EUR PRICES
+  r = requests.get("https://min-api.cryptocompare.com/data/price?fsym="+crypto_name+"&tsyms=BTC,USD,EUR")
+
+  return jsonify(
+    status=200,
+    replies=[{
+      'type': 'text',
+      'content': '%s 的价格是 %s is :\n%f BTC, \n%f USD, and \n%f EUR.' % (crypto_name, r.json()['BTC'], r.json()['USD'], r.json()['EUR'])
+    }]
+  )
+
 @app.route('/view', methods=['POST'])
 def view():
   print(port)
