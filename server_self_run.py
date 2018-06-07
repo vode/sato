f82424a4bd438d4c05f8b0bba1d660a3ed3ae032
vode <vode@vodedeMacBook-Pro.local>
2018-06-07 22:41:01 +0800

    bug fix

--- server_self_run.py
+++ server_self_run.py
@@ -1,19 +1,15 @@
 from flask import Flask, request, jsonify
 import json
 import requests
-
+import traceback
 app = Flask(__name__)
-port = '5000'
-app.run(port=port)
+port = '5001'
 
 from flask import Flask, request, jsonify
 import json
 import requests
 import os
 import re
-app = Flask(__name__)
-port = int(os.environ["PORT"])
-print(port)
 
 mapper = {
   "比特币": "BTC",
@@ -24,8 +20,8 @@ mapper = {
 }
 r = requests.get("https://www.cryptocompare.com/api/data/coinlist/")
 info_dic = r.json()["Data"]
-
 def is_crypto(name):
+  print(name)
   if name in mapper or name in info_dic:
     return True
   else:
@@ -45,32 +41,39 @@ def match(name):
 
 def get_name(data):
   try:
-    for crypto in crypto_list.keys():
+    for crypto in info_dic.keys():
       if crypto in data['nlp']['source']:
+        print('######')
         return format(crypto)
-    for key in mapper:
+    for key in mapper.keys():
+      print(key,data['nlp']['source'])
       if key in data['nlp']['source']:
+        print('!!!!',mapper[key])
         return mapper[key]
   except Exception:
+    print(traceback.format_exc())
     return ""
 
-def get_query(param):
+def get_query(data):
   return data['nlp']['source']
 
 def gen_crypto_info(crypto_name):
+  print(crypto_name)
   if is_crypto(crypto_name):
     try:
-      r = requests.get("https://min-api.cryptocompare.com/data/pricemultifull??fsym="+crypto_name+"&tsyms=USD")
+      r = requests.get("https://min-api.cryptocompare.com/data/pricemultifull?fsyms="+crypto_name+"&tsyms=USD")
+      print(r)
       price =  r.json()['DISPLAY'][crypto_name]["USD"]["PRICE"]
       openday = r.json()['DISPLAY'][crypto_name]["USD"]["OPENDAY"]
       highday = r.json()['DISPLAY'][crypto_name]["USD"]["HIGHDAY"]
       lowday = r.json()['DISPLAY'][crypto_name]["USD"]["LOWDAY"]
       market = r.json()['DISPLAY'][crypto_name]["USD"]["LASTMARKET"]
-      change24 = r.json()['DISPLAY'][crypto_name]["USD"]["CHANGEPCT24HOUR"]
+      change24 = r.json()['DISPLAY'][crypto_name]["USD"]["CHANGE24HOUR"]
       changeday = r.json()['DISPLAY'][crypto_name]["USD"]["CHANGEDAY"]
       official_name = get_official_name(crypto_name)
-      return '货币代号: %s\n 货币全称:%s\n 实时价格: %s\n 今日最高价：%s\n 今日最低价: %s\n 24小时涨幅: %s\n 今日涨幅: %s\n 来源交易所: %s' %(crypto_name,official_name, price,openday,highday,lowday,change24,changeday,market)
+      return ' 货币代号: %s\n 货币全称:%s\n 实时价格: %s\n 今日最高价: %s\n 今日最低价: %s\n 24小时涨幅: %s\n 今日涨幅: %s\n 来源交易所: %s' %(crypto_name,official_name, price,highday,lowday,change24,changeday,market)
     except Exception:
+      print(traceback.format_exc())
       return "不好意思，小火查不到 %s 的行情呢" % (crypto_name)
   else:
     return "不好意思，小火找不到您说的数字货币呢"
@@ -84,12 +87,14 @@ def get_official_name(crypto_name):
 
 @app.route('/last', methods=['POST'])
 def last():
-  data = json.loads(request.get_data())
+  data = json.loads(request.get_data(as_text=True))
+  print(data)
   query = get_query(data)
   response_text = ""
   if("火币网" in query):
     response_text = "火币集团是全球领先的数字资产金融服务商。2013年，火币创始团队看到了区块链行业的巨大发展潜力，心怀推动全球新金融改革的愿景，创立火币集团。火币集团以“让金融更高效，让财富更自由”作为集团使命，秉承“用户至上”的服务理念，致力于为全球用户提供安全、专业、诚信、优质的服务。目前，火币集团已完成对新加坡、美国、日本、韩国、香港等多个国家及地区的布局。"
   elif(is_crypto(query)):
+    query=get_name(data)
     response_text = gen_crypto_info(query)
   else:
     response_text = "不好意思，小火不太明白你的问题呢\n你可以尝试提问：\n推荐群\n比特币的价格\n如何下载火币App\n火币网简介\n"
@@ -105,10 +110,11 @@ def last():
 def price():
   print(port)
   
-  data = json.loads(request.get_data())
+  data = json.loads(request.get_data(as_text=True))
   # FETCH THE CRYPTO NAME
   crypto_name = get_name(data)
-  response_text = gen_crypto_info()
+  print(crypto_name)
+  response_text = gen_crypto_info(crypto_name)
   return jsonify(
     status=200,
     replies=[{
@@ -153,6 +159,9 @@ def test_view():
     }]
   )
 
+@app.route('/',methods=['GET'])
+def index():
+  return jsonify(status=200,replies=[{'type':'text','content':'hello world'}])
 @app.route('/view', methods=['POST'])
 def view():
   print(port)
@@ -176,4 +185,5 @@ def view():
 def errors():
   print(json.loads(request.get_data()))
   return jsonify(status=200)
-app.run(port=port)
+app.debug=True
+app.run(port=port,debug=True)
