fbb5d81e01da6a97b7717fcf7fbf4e7ad92fd1f0
vode <vode@vodedeMacBook-Pro.local>
2018-06-09 18:35:58 +0800

    test

--- server.py
+++ server.py
@@ -198,7 +198,7 @@ def get_huobi_info(crypto_name):
     lowday = ticker['tick']['low']
     amount = int(ticker['tick']['amount'])
     official_name = get_official_name(crypto_name)
-    return ' 货币代号: %s\n 货币全称:%s\n 实时价格: %s$\n 今日最高价: %s$\n 今日最低价: %s$\n 来源交易所: %s' %(crypto_name,official_name,price,highday,lowday,'Huobi')
+    return ' 货币代号: %s\n 货币全称:%s\n 实时价格: %s$\n 今日最高价: %s$\n 今日最低价: %s$\n 来源交易所: %s' %(crypto_name,official_name,price,highday,lowday,'HuobiPro')
   except Exception:
     print(traceback.format_exc())
     return None
@@ -239,13 +239,17 @@ def last():
   print(data)
   query = get_query(data)
   response_text = ""
-  if("火币网" in query):
-    response_text = "火币集团是全球领先的数字资产金融服务商。2013年，火币创始团队看到了区块链行业的巨大发展潜力，心怀推动全球新金融改革的愿景，创立火币集团。火币集团以“让金融更高效，让财富更自由”作为集团使命，秉承“用户至上”的服务理念，致力于为全球用户提供安全、专业、诚信、优质的服务。目前，火币集团已完成对新加坡、美国、日本、韩国、香港等多个国家及地区的布局。"
-  elif(get_name(data)!=""):
-    query=get_name(data)
-    response_text = get_market_price(query)
-  else:
-    response_text = "不好意思，小火不太明白你的问题呢\n你可以尝试提问：\n推荐群\n比特币的价格\n如何下载火币App\n火币网简介\n"
+  try:
+    if("火币网" in query):
+      response_text = "火币集团是全球领先的数字资产金融服务商。2013年，火币创始团队看到了区块链行业的巨大发展潜力，心怀推动全球新金融改革的愿景，创立火币集团。火币集团以“让金融更高效，让财富更自由”作为集团使命，秉承“用户至上”的服务理念，致力于为全球用户提供安全、专业、诚信、优质的服务。目前，火币集团已完成对新加坡、美国、日本、韩国、香港等多个国家及地区的布局。"
+    elif(get_name(data)!=""):
+      query=get_name(data)
+      response_text = get_market_price(query)
+    else:
+      response_text = "不好意思，小火不太明白你的问题呢\n你可以尝试提问：\n推荐群\n比特币的价格\n如何下载火币App\n火币网简介\n"
+    
+  except Exception:
+    response_text = traceback.format_exc()
   return jsonify(
       status=200,
       replies=[{
