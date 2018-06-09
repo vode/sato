23d73141e5bfeea657c21ad226ad427649c1dd15
vode <vode@vodedeMacBook-Pro.local>
2018-06-09 17:25:11 +0800

    back to py2

--- server.py
+++ server.py
@@ -7,70 +7,85 @@ app = Flask(__name__)
 port = int(os.environ["PORT"])
 print(port)
 
-import base64
-import datetime
-import hashlib
-import hmac
-import json
 import urllib
-import urllib.parse
-import urllib.request
+import datetime
 import requests
+import urllib2
+import urlparse
 
+# timeout in 5 seconds:
+TIMEOUT = 5
+
+API_HOST = "api.huobi.pro"
+
+SCHEME = 'https'
+
+# language setting: 'zh-CN', 'en':
+LANG = 'zh-CN'
+
+DEFAULT_GET_HEADERS = {
+    'Accept': 'application/json',
+    'Accept-Language': LANG,
+    'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:53.0) Gecko/20100101 Firefox/53.0'
+}
+
+DEFAULT_POST_HEADERS = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+    'Accept-Language': LANG,
+    'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:53.0) Gecko/20100101 Firefox/53.0'
+}
 # 此处填写APIKEY
 
 ACCESS_KEY = "d2e82710-03c51810-9fed7063-7ba8f"
 SECRET_KEY = "b12fe6f4-dccb03b3-54fcdf43-bda24"
 
 
-
-# API 请求地址
-MARKET_URL = "https://api.huobi.pro"
-TRADE_URL = "https://api.huobi.pro"
-
 # 首次运行可通过get_accounts()获取acct_id,然后直接赋值,减少重复获取。
 ACCOUNT_ID = None
 
-#'Timestamp': '2017-06-02T06:13:49'
 
+# API 请求地址
+MARKET_URL = TRADE_URL = "https://api.huobi.pro"
+
+#各种请求,获取数据方式
 def http_get_request(url, params, add_to_headers=None):
     headers = {
         "Content-type": "application/x-www-form-urlencoded",
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36',
+        'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:53.0) Gecko/20100101 Firefox/53.0'
     }
     if add_to_headers:
         headers.update(add_to_headers)
-    postdata = urllib.parse.urlencode(params)
-    response = requests.get(url, postdata, headers=headers, timeout=5) 
+    postdata = urllib.urlencode(params)
     try:
-        
+        response = requests.get(url, postdata, headers=headers, timeout=TIMEOUT)
         if response.status_code == 200:
             return response.json()
         else:
-            return
-    except BaseException as e:
-        print("httpGet failed, detail is:%s,%s" %(response.text,e))
-        return
-
+            return {"status":"fail"}
+    except Exception as e:
+        print("httpGet failed, detail is:%s" %e)
+        return {"status":"fail","msg":e}
 
 def http_post_request(url, params, add_to_headers=None):
     headers = {
         "Accept": "application/json",
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
+        "User-Agent": "Chrome/39.0.2171.71",
+        'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:53.0) Gecko/20100101 Firefox/53.0'
     }
     if add_to_headers:
         headers.update(add_to_headers)
     postdata = json.dumps(params)
-    response = requests.post(url, postdata, headers=headers, timeout=10)
     try:
-        
+        response = requests.post(url, postdata, headers=headers, timeout=TIMEOUT)
         if response.status_code == 200:
             return response.json()
         else:
-            return
-    except BaseException as e:
-        print("httpPost failed, detail is:%s,%s" %(response.text,e))
-        return
+            return response.json()
+    except Exception as e:
+        print("httpPost failed, detail is:%s" % e)
+        return {"status":"fail","msg":e}
 
 
 def api_key_get(params, request_path):
@@ -81,11 +96,12 @@ def api_key_get(params, request_path):
                    'SignatureVersion': '2',
                    'Timestamp': timestamp})
 
-    host_url = TRADE_URL
-    host_name = urllib.parse.urlparse(host_url).hostname
+    host_name = host_url = TRADE_URL
+    host_name = urlparse.urlparse(host_url).hostname
     host_name = host_name.lower()
-    params['Signature'] = createSign(params, method, host_name, request_path, SECRET_KEY)
 
+
+    params['Signature'] = createSign(params, method, host_name, request_path, SECRET_KEY)
     url = host_url + request_path
     return http_get_request(url, params)
 
@@ -99,25 +115,23 @@ def api_key_post(params, request_path):
                       'Timestamp': timestamp}
 
     host_url = TRADE_URL
-    host_name = urllib.parse.urlparse(host_url).hostname
+    host_name = urlparse.urlparse(host_url).hostname
     host_name = host_name.lower()
     params_to_sign['Signature'] = createSign(params_to_sign, method, host_name, request_path, SECRET_KEY)
-    url = host_url + request_path + '?' + urllib.parse.urlencode(params_to_sign)
+    url = host_url + request_path + '?' + urllib.urlencode(params_to_sign)
     return http_post_request(url, params)
 
 
 def createSign(pParams, method, host_url, request_path, secret_key):
     sorted_params = sorted(pParams.items(), key=lambda d: d[0], reverse=False)
-    encode_params = urllib.parse.urlencode(sorted_params)
+    encode_params = urllib.urlencode(sorted_params)
     payload = [method, host_url, request_path, encode_params]
     payload = '\n'.join(payload)
     payload = payload.encode(encoding='UTF8')
     secret_key = secret_key.encode(encoding='UTF8')
-
     digest = hmac.new(secret_key, payload, digestmod=hashlib.sha256).digest()
     signature = base64.b64encode(digest)
     signature = signature.decode()
-    return signature
 
 # 获取merge ticker
 def get_ticker(symbol):
